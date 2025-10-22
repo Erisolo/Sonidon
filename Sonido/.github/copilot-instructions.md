30eded6f2c64ceda9d087c99bc7f51e2dfd9f1bb
Erisolo <marsolor@ucm.es>
2025-10-22 19:00:04 +0200

    ale

--- Sonido/.github/copilot-instructions.md
+++ Sonido/.github/copilot-instructions.md
@@ -1,8 +1,4 @@
 <!-- UNITY CODE ASSIST INSTRUCTIONS START -->
 - Project name: Sonido
 - Unity version: Unity 2021.3.45f1
-- Active game object:
-  - Name: GameObject
-  - Tag: Untagged
-  - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Sonido/Assets/Scripts/Slice.cs
+++ Sonido/Assets/Scripts/Slice.cs
@@ -5,32 +5,59 @@ using UnityEngine;
 public class SchedEvent: MonoBehaviour {
     private AudioSource head;
     private AudioSource tail;
-    //public AudioClip sound01, sound02;
-    public AudioClip[] pcmDataHeads, pcmDataTails;
-    private int nHeads, nTails;
+    private AudioSource casings;
+
+    [SerializeField]
+    private float lap;
+    [SerializeField]
+    private float overlapTime;
+
+    public AudioClip[] pcmDataHeads, pcmDataTails, pcmDataCasings;
+    private int nHeads, nTails, nCasings;
 
 
     void Awake(){
         nHeads = pcmDataHeads.Length;
         nTails = pcmDataTails.Length;
+        nCasings = pcmDataCasings.Length;
         head = gameObject.AddComponent<AudioSource>();        
-        tail = gameObject.AddComponent<AudioSource>();        
+        tail = gameObject.AddComponent<AudioSource>();
+        casings = gameObject.AddComponent<AudioSource>();        
     }
 
-    void Start(){
+    private void Start()
+    {
+        for (int i = 0; i< pcmDataHeads.Length; i++) 
+        {
+        }
+        for (int i = 0; i< pcmDataHeads.Length; i++) 
+        {
+        }
+        for (int i = 0; i< pcmDataHeads.Length; i++) 
+        {
+        }
+    }
+
+    void FadeIn(AudioClip clip)
+    {
+
+    }
+    void FadeOut(AudioClip clip)
+    {
+
     }
 
     // Update is called once per frame
     void Update(){
         if (Input.GetKeyDown(KeyCode.Alpha3)) {
-            int h = Random.Range(0, nHeads), t = Random.Range(0, nTails);
+            int h = Random.Range(0, nHeads), t = Random.Range(0, nTails), c = Random.Range(0, nCasings);
             head.clip = pcmDataHeads[h];
             tail.clip = pcmDataTails[t];
+            casings.clip = pcmDataCasings[c];
             
             double clipLength = (double) head.clip.samples / head.pitch;
             
             int sRATE = AudioSettings.outputSampleRate;
-            Debug.Log($"head {h} length {clipLength}  p tail {t}  sRATE: {sRATE}");
 
             head.Play();
             tail.PlayScheduled(AudioSettings.dspTime+clipLength/sRATE);
