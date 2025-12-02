fb1106bdf880f31672cfba78d498ad3b8bc35023
David-104 <dcanelad@ucm.es>
2025-12-02 20:05:34 +0100

    volumen radio y mas cosas yo que se

--- Sonido/Assets/Radio.cs
+++ Sonido/Assets/Radio.cs
@@ -9,15 +9,19 @@ public class Radio : MonoBehaviour
     private StudioEventEmitter emitter;
     [SerializeField][Range(0.0f, 1.0f)]
     private float emisora;
-
+    [SerializeField][Range(0.0f, 1.0f)]
+    private float volumen = 0.8f;
+    
     void Start()
     {
         emitter = GetComponent<StudioEventEmitter>();
+        
     }
     
     // Update is called once per frame
     void Update()
     {
         emitter.SetParameter("Emisora", emisora);
+        emitter.SetParameter("Volumen Radio", volumen);
     }
 }
