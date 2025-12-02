9b21851cdd20d5e079199f1bb2b41078dd509f26
David-104 <dcanelad@ucm.es>
2025-12-02 17:03:31 +0100

    radio (no el de marie curie)

---
+++ Sonido/Assets/Radio.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using FMODUnity;
+using UnityEngine;
+
+public class Radio : MonoBehaviour
+{
+
+    private StudioEventEmitter emitter;
+    [SerializeField][Range(0.0f, 1.0f)]
+    private float emisora;
+
+    void Start()
+    {
+        emitter = GetComponent<StudioEventEmitter>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        emitter.SetParameter("Emisora", emisora);
+    }
+}
